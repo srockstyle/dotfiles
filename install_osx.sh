a493ff3e3328e2eacaf29a004929714602ddf25a
Shohei Kobayashi <kobayashi_shohei@srockstyle.com>
2019-09-10 05:57:38 +0900

    最初のやつ

--- install_osx.sh
+++ install_osx.sh
@@ -21,10 +21,12 @@ brew install zsh
 brew install ghq
 brew install peco
 brew install hub
+brew install lua
 brew install vim --with-python3 --with-lua
 brew install ctags
-brew install node
-ctags -R
+brew install yarn --ignore-dependencies
+# brew install node
+# ctags -R
 
 ##-------------
 ## vim
@@ -51,11 +53,12 @@ gem install solargraph
 rm -rf ~/.zshrc
 rm -rf ~/.zsh
 mkdir ~/.zsh
+mkdir ~/.cache/shell
 ln -s $CURRENT_PATH/zshrc/main.zsh ~/.zshrc
 ln -s $CURRENT_PATH/zshrc/zsh.d ~/.zsh/functions
 git clone git://github.com/zsh-users/zaw.git ~/.zsh/functions/zaw
 git clone git://github.com/mollifier/cd-gitroot.git ~/.zsh/functions/cd-gitroot
-
+git clone git://github.com/yyuu/pyenv.git ~/.pyenv
 
 ## font
 brew tap sanemat/font
@@ -66,14 +69,12 @@ cp -rf /usr/local/Cellar/ricty/*/share/fonts/Ricty*.ttf ~/Library/Fonts/
 ## git
 git config --global user.name srockstyle
 git config --global user.email kobayashi_shohei@srockstyle.com
-git log --graph --all --format="%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s"
+git config --global alias.tree 'log --graph --all --format="%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s"'
 
 ## Brewでいれるやつ2
 brew install wget
 brew install tree
 brew install go
-
-# インフラ・ネットワーク系
 brew install wget
 brew install htop
 brew install nkf
@@ -95,28 +96,8 @@ brew install rails-completion
 brew install grunt-completion
 brew install django-completion
 brew install pip-completion
-
-## 開発環境
-# brew cask install virtualbox
-# brew cask install vagrant
-# brew cask install chefdk
-
-#brew cask install evernote
-#brew cask install skitch
-
-## 開発環境もろもろ
 gem install ec2ssh
 ec2ssh init
 ec2ssh update
 gem install kitchen-vagrant
-
-## Atom
-apm install rsense
-apm install japanese-menu
-apm install autocomplete-plus
-
-# desktop apps via cask
 brew cask install cheatsheet
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install slack
